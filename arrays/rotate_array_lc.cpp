308061e0e6a34580e524344b62f4f2241c2ac450
Ayush Gupta <ayushgupta2429.ag@gmail.com>
2022-05-27 23:49:56 +0530

    rotating array using mod operator

---
+++ arrays/rotate_array_lc.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> tmp(nums.size());
+        for(int i=0;i<nums.size();i++){
+            tmp[(i+k)%nums.size()]=nums[i];
+        }
+        nums=tmp;
+    }
+};
