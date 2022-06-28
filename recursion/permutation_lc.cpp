1b4d9ee070fa620a756406e4ae98575be83b0e87
Ayush Gupta <ayushgupta2429.ag@gmail.com>
2022-06-28 13:31:14 +0530

    permutation of strings using recursion

---
+++ recursion/permutation_lc.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     void solve(vector<vector<int>> &ans,vector<int> nums,int index){
+        //base case
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int j=index;j<nums.size();j++){
+            swap(nums[index],nums[j]);
+            solve(ans,nums,index+1);
+            //backtracking
+            swap(nums[index],nums[j]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index=0;
+        solve(ans,nums,index);
+        return ans;
+    }
+};
