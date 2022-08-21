b247b62a869ba09b679f2ab7b4faad0ef2a98900
Ayush Gupta <ayushgupta2429.ag@gmail.com>
2022-08-22 00:21:33 +0530

    using same lvl order traversal and map<int,int>,queue = pair(node*,int)

---
+++ binary_trees/top_view_gfg.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        //Your code here
+        map<int,int> topnode;
+        vector<int> ans;
+        
+        if(root == NULL){
+            return ans;
+        }
+        
+        queue<pair<Node*,int>> q;
+        //adding first node in queue.
+        q.push(make_pair(root,0));
+        
+        while(!q.empty()){
+            //phela node nikala queue ka joki pair type ka tha.
+            pair<Node*,int> tmp = q.front();
+            q.pop();
+            
+            Node* top = tmp.first;
+            int hd = tmp.second;
+            
+            //checking if there is not found any existing hd mapping.
+            if(topnode.find(hd) == topnode.end()){
+                topnode[hd] = top -> data;
+            }
+            
+            //now traversing left and right.
+            if(top -> left){
+                q.push(make_pair(top -> left,hd-1));
+            }
+            if(top -> right){
+                q.push(make_pair(top -> right,hd+1));
+            }
+        }
+        
+        //now traverse in the map for printing the values seen from top.
+        for(auto i:topnode){
+            //i.second is basically the node->data value mapped at particular hd.
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+
+};
