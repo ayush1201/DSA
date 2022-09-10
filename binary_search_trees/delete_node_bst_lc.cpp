62519277ca387028899da6fb2e5e64a13b9b933e
Ayush Gupta <ayushgupta2429.ag@gmail.com>
2022-09-10 21:44:28 +0530

    delete node bst ,4 conditions and in both children case just copy root -> right smallest in the root and root -> right is called in delete function,mini and recursion do rest

---
+++ binary_search_trees/delete_node_bst_lc.cpp
@@ -0,0 +1,53 @@
+//algo is just check whether given node is in left part or in right part or it is the root itself. if found ,then it has four states 0,1,2 children but the catch is in 2 children -left,right. So in that case just check lowest node in root->right and simply copy that in root node and store it in mini variable and root -> right is given in delete call to delete mini and return root.
+ class Solution {
+public:
+    
+    TreeNode* getMin(TreeNode* root){
+        //temp is minimum node from that root.
+        TreeNode* temp = root ;
+        while((temp -> left)){
+            temp = temp -> left;
+        }
+        return temp;
+    }
+    
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        //babbar approach.
+        //base case
+        if(root == NULL){
+            return root;
+        }
+        if(root -> val == key){
+            //if no child.
+            if(!(root -> left) && !(root -> right)){
+                return NULL;
+            }
+            //means left child is not null
+            else if((root -> left) && !(root -> right)){
+                return root -> left;
+            }
+            else if(!(root -> left) && (root -> right)){
+                return root -> right;
+            }
+            else{
+                //do child hai aur isi ko handle karna hai.
+                //root -> right me jaao aur smallest element ko root me copy kardo.
+                TreeNode* rightNode = root -> right;
+                int mini = getMin(rightNode) -> val;
+                root -> val = mini;
+                //now call delete function and delete the mini node .
+                root -> right = deleteNode(root -> right,mini);
+                return root;
+            }
+        }
+        else if(root -> val > key){
+            //left me jao
+            root -> left = deleteNode(root -> left,key);
+        }
+        else{
+            //right me jao
+            root -> right = deleteNode(root -> right,key);
+        }
+       return root;
+    }
+};
