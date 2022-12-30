41358673d3e2e33c3d03c645f3e7e7aee6143294
Ayush Gupta <ayushgupta2429.ag@gmail.com>
2022-12-30 19:36:04 +0530

    dp ka ek box show karra hai ki vaha tak ke subarrays jo AP hai .

---
+++ dp/arithmetic_slices_lc.cpp
@@ -0,0 +1,20 @@
+//dp ka ek box show karra hai ki vaha tak ke subarrays jo AP hai .
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,0);
+        if(n <= 2){
+            return 0;
+        }
+
+        int ans = 0;
+        for(int i=2;i<n;i++){
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]){
+                dp[i] = dp[i-1] + 1;
+                ans += dp[i];
+            }
+        }
+        return ans;
+    }
+};
